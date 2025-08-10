4bbbacd5e792e34f4effeda0d7d549257b6195b8
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-11 04:53:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/travel-journal-react-project/",
-=======
   base: "/travel-journal-react-project",
->>>>>>> 760418c9ecb536922ae548858e9dee29e1b963a1
   plugins: [react()],
 })
