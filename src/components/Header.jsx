8778d769fee28813f65e3a419ca4020d21b24ee6
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-11 05:19:44 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header>
             <nav>
-                <img src="src/assets/pr2-logo.png" alt="logo" />
+                <img src="https://cdn-icons-png.freepik.com/256/315/315274.png?semt=ais_white_label" alt="logo" />
                 <h1>My Travel Journal</h1>
             </nav>
         </header>
